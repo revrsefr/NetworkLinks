17ebf1ae5672430da88d81a1ef4efb1c6e17309b
Jean <mike.chevronnet@gmail.com>
2026-06-25 08:43:29 +0000

    corecommands: delegate (un)loading to control helpers
    load, unload, reload, reloadcore and rehash now call the shared helpers
    in control instead of duplicating the teardown logic. rehash reports any
    module that failed to reload.

--- coremods/corecommands.py
+++ coremods/corecommands.py
@@ -4,8 +4,6 @@ corecommands.py - Implements core NetLink commands.
 
 from __future__ import annotations
 
-import gc
-import importlib
 import sys
 
 from netlink import utils, world
@@ -14,24 +12,6 @@ from netlink.log import log
 from . import control, permissions
 
 __all__ = []
-
-
-def _remove_module_commands_hooks(modulename):
-    """Removes all commands and hooks registered by the given module name."""
-    cmds = world.services['netlink'].commands
-    for cmdname, cmdfuncs in cmds.copy().items():
-        for cmdfunc in cmdfuncs.copy():
-            if cmdfunc.__module__ == modulename:
-                cmds[cmdname].remove(cmdfunc)
-        if not cmds[cmdname]:
-            del cmds[cmdname]
-
-    for hookname, hookpairs in world.hooks.copy().items():
-        for hookpair in hookpairs.copy():
-            if hookpair[1].__module__ == modulename:
-                world.hooks[hookname].remove(hookpair)
-        if not world.hooks[hookname]:
-            del world.hooks[hookname]
 
 # Essential, core commands go here so that the "commands" plugin with less-important,
 # but still generic functions can be reloaded.
@@ -64,17 +44,13 @@ def load(irc, source: str, args: list):
         return
     log.info('(%s) Loading plugin %r for %s', irc.name, name, irc.get_hostmask(source))
     try:
-        world.plugins[name] = pl = utils._load_plugin(name)
+        control.load_plugin(name, irc=irc)
     except ImportError as e:
         if str(e) == ('No module named %r' % name):
             log.exception('Failed to load plugin %r: The plugin could not be found.', name)
         else:
             log.exception('Failed to load plugin %r: ImportError.', name)
         raise
-    else:
-        if hasattr(pl, 'main'):
-            log.debug('Calling main() function of plugin %r', pl)
-            pl.main(irc=irc)
     irc.reply("Loaded plugin %r." % name)
 
 @utils.add_cmd
@@ -90,59 +66,9 @@ def unload(irc, source: str, args: list):
         irc.reply("Error: Not enough arguments. Needs 1: plugin name.")
         return
 
-    # Since we're using absolute imports in 0.9.x+, the module name differs from the actual plugin
-    # name.
-    modulename = utils.PLUGIN_PREFIX + name
-
     if name in world.plugins:
         log.info('(%s) Unloading plugin %r for %s', irc.name, name, irc.get_hostmask(source))
-        pl = world.plugins[name]
-        log.debug('sys.getrefcount of plugin %s is %s', pl, sys.getrefcount(pl))
-
-        # Remove any command functions defined by the plugin.
-        for cmdname, cmdfuncs in world.services['netlink'].commands.copy().items():
-            log.debug('cmdname=%s, cmdfuncs=%s', cmdname, cmdfuncs)
-
-            for cmdfunc in cmdfuncs:
-                log.debug('__module__ of cmdfunc %s is %s', cmdfunc, cmdfunc.__module__)
-                if cmdfunc.__module__ == modulename:
-                    log.debug("Removing %s from world.services['netlink'].commands[%s]", cmdfunc, cmdname)
-                    world.services['netlink'].commands[cmdname].remove(cmdfunc)
-
-                    # If the cmdfunc list is empty, remove it.
-                    if not cmdfuncs:
-                        log.debug("Removing world.services['netlink'].commands[%s] (it's empty now)", cmdname)
-                        del world.services['netlink'].commands[cmdname]
-
-        # Remove any command hooks set by the plugin.
-        for hookname, hookpairs in world.hooks.copy().items():
-            for hookpair in hookpairs:
-                hookfunc = hookpair[1]
-                if hookfunc.__module__ == modulename:
-                    log.debug('Trying to remove hook func %s (%s) from plugin %s', hookfunc, hookname, modulename)
-                    world.hooks[hookname].remove(hookpair)
-                    # If the hookfuncs list is empty, remove it.
-                    if not hookpairs:
-                        del world.hooks[hookname]
-
-        # Call the die() function in the plugin, if present.
-        if hasattr(pl, 'die'):
-            try:
-                pl.die(irc=irc)
-            except:  # But don't allow it to crash the server.
-                log.exception('(%s) Error occurred in die() of plugin %s, skipping...', irc.name, pl)
-
-        # Delete it from memory (hopefully).
-        del world.plugins[name]
-        for n in (name, modulename):
-            if n in sys.modules:
-                del sys.modules[n]
-            if n in globals():
-                del globals()[n]
-
-        # Garbage collect.
-        gc.collect()
-
+        control.unload_plugin(name, irc=irc)
         irc.reply("Unloaded plugin %r." % name)
         return True  # We succeeded, make it clear (this status is used by reload() below)
     else:
@@ -163,12 +89,6 @@ def reload(irc, source: str, args: list):
     if unload(irc, source, args):
         load(irc, source, args)
 
-# Coremods that can't be reloaded in place: permissions holds the
-# default_permissions registry populated by other modules (reloading wipes it),
-# and service_support registers the main service at import (reloading re-runs
-# register_service and raises "already bound").
-_UNRELOADABLE_COREMODS = {'permissions', 'service_support'}
-
 @utils.add_cmd
 def reloadcore(irc, source: str, args: list):
     """<coremod name>.
@@ -187,39 +107,38 @@ def reloadcore(irc, source: str, args: list):
     if modulename not in sys.modules:
         irc.error("Unknown or unloaded coremod %r." % name)
         return
-    if name in _UNRELOADABLE_COREMODS:
+    if name in control.UNRELOADABLE_COREMODS:
         irc.error("Coremod %r can't be reloaded at runtime (it holds shared state)." % name)
         return
 
-    module = sys.modules[modulename]
     log.info('(%s) Reloading coremod %r for %s', irc.name, name, irc.get_hostmask(source))
-    _remove_module_commands_hooks(modulename)
     try:
-        importlib.reload(module)
+        control.reload_coremod(name, irc=irc)
     except Exception as e:
         log.exception('(%s) Failed to reload coremod %r', irc.name, name)
         irc.error("Failed to reload coremod %r: %s: %s" % (name, type(e).__name__, e))
         return
-    if hasattr(module, 'main'):
-        module.main(irc=irc)
     irc.reply("Reloaded coremod %r." % name)
 
 @utils.add_cmd
 def rehash(irc, source: str, args: list):
     """takes no arguments.
 
-    Reloads the configuration file for NetLink, (dis)connecting added/removed networks.
-
-    Note: plugins must be manually reloaded."""
+    Reloads the configuration file along with all configured plugins and coremods,
+    and (dis)connects added/removed networks -- all in place, without dropping the
+    links that are still configured."""
     permissions.check_permissions(irc, source, ['core.rehash'])
     try:
-        control.rehash()
-    except Exception as e:  # Something went wrong, abort.
+        errors = control.rehash()
+    except Exception as e:  # Something went wrong loading the config, abort.
         irc.reply("Error loading configuration file: %s: %s" % (type(e).__name__, e))
         return
+
+    irc.announce_administration("%s rehashed the NetLink configuration." % irc.get_hostmask(source))
+    if errors:
+        irc.reply("Rehash complete with %d error(s): %s" % (len(errors), '; '.join(errors)))
     else:
-        irc.announce_administration("%s rehashed the NetLink configuration." % irc.get_hostmask(source))
-        irc.reply("Done.")
+        irc.reply("Done. Reloaded config, plugins and coremods; links left intact.")
 
 @utils.add_cmd
 def clearqueue(irc, source: str, args: list):
